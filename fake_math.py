323e5e8e72563c618a28c0823c144434f4b6b9cf
Egor <repingign@gmail.com>
2024-08-23 21:29:03 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,10 @@
+
+def divide(ferst, second):
+    if second ==0:
+         return 'Ошибка'
+    else:
+        return ferst / second
+
+
+print(divide(69, 3))
+print(divide(3, 0))
---
+++ true_math.py
@@ -0,0 +1,10 @@
+from math import inf
+
+def divide(ferst, second):
+    if second == 0:
+        return inf
+    else:
+        return ferst / second
+
+# print(divide(49, 7))
+# print(divide(15, 0))
